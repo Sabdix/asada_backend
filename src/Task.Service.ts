abccaf29aa20176e26ae250cbecb5558fc442a5f
DESKTOP-HL62M51\Andres <amakirx@gmail.com>
2025-04-23 08:59:59 -0600

    -- se implementa el log para describir el funcionamiento del cron job

--- src/Task.Service.ts
+++ src/Task.Service.ts
@@ -10,9 +10,9 @@ export class TasksService {
   constructor(
     private checkListUserService: CheckListUserService,
     private checkListHistoryService: CheckListHistoryService,
-  ) { 
+  ) {
     this.checkListUserService = checkListUserService,
-    this.checkListHistoryService = checkListHistoryService 
+      this.checkListHistoryService = checkListHistoryService
   }
 
 
@@ -20,9 +20,12 @@ export class TasksService {
   @Cron(CronExpression.EVERY_DAY_AT_1AM)
   //@Cron(CronExpression.EVERY_30_SECONDS)
   async handleCreateTodayHistory() {
+    this.logger.log('Incia ejecucion del cronjob');
     const weekDay = new Date().getDay();
+    this.logger.log('Se obtiene el weekday actual: ' + weekDay.toString());
 
     const checkListOfTheDay = await this.checkListUserService.getCheckListByWeekDay(weekDay)
+    this.logger.log('Se obtienen todos los checklist a los que se les generará un registro en el historial');
 
     for (const checkList of checkListOfTheDay) {
       const checkListHistory = new CreateCheckListHistoryRequestDto
@@ -30,8 +33,10 @@ export class TasksService {
       checkListHistory.uuid_check_list = checkList.uuid_check_list
       checkListHistory.uuid_user = checkList.uuid_user
       checkListHistory.date = new Date()
+      this.logger.log('Se asigana la data previo a la insecion para el registro del checklist user: ' + checkList.uuid.toString() + ' del usuario: ' + checkList.uuid_user.toString());
 
-      await this.checkListHistoryService.creteCheckListHistory(checkListHistory,checkList)      
+      const newHistory = await this.checkListHistoryService.creteCheckListHistory(checkListHistory, checkList)
+      this.logger.log('Se inserto con exito el history:  ' + newHistory.uuid.toString() + ' correspondiente al checklist user: ' + checkList.uuid.toString());
     }
 
   }
