d81917ea2c243ec2731ee187009c3cf4cff3e8a4
DESKTOP-HL62M51\Andres <amakirx@gmail.com>
2025-05-02 12:59:06 -0600

    -- se quita el console.log

--- src/checkList/application/queries/getCheckListHistory/getCheckListHistory.query.handler.ts
+++ src/checkList/application/queries/getCheckListHistory/getCheckListHistory.query.handler.ts
@@ -15,8 +15,6 @@ export class GetCheckListHistoryQueryHandler implements IQueryHandler<GetCheckLi
     async execute() {
         const checkListHistory = await this.checkListHistoryService.getAllCheckListHistory();
 
-        console.log(checkListHistory[0])
-
         return WsResponse.buildOkResponse(
             plainToInstance(CheckListHistoryDto, checkListHistory, { excludeExtraneousValues: true }),
         );
